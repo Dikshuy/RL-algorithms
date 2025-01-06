6fb06781d8aa29c96fff7f00b309e79eedff619d
Dikshuy <dikshant1212hsr@gmail.com>
2025-01-05 21:18:58 -0700

    sac base code added

---
+++ sac/main.py
@@ -0,0 +1,44 @@
+import numpy as np
+import gymnasium as gym
+from sac import SAC, NormalizedActions
+
+env=gym.make('CartPole-v1')
+env = NormalizedActions(env)
+n_action=env.action_space.n
+n_state=env.observation_space.shape[0]
+
+alpha=0.0003
+gamma=0.99
+device='cpu'
+capacity=10000
+gradient_steps=1
+batch_size=128
+tau=0.005
+
+iterations = 100000
+log_interval = 2000
+
+load = False
+
+sac_agent = SAC(n_state, n_action, alpha, gamma, capacity, gradient_steps, batch_size, tau, device)
+
+if load:    sac_agent.load_models()
+
+
+for i in range(iterations):
+    obs, _ = env.reset()
+    done = False
+    episode_reward = 0
+    while not done:
+        action = sac_agent.choose_action(obs)
+        next_state, reward, terminated, truncated, _ = env.step(action)
+        done = terminated or truncated
+        sac_agent.store_transition(obs, action, reward, next_state, done)
+        if sac_agent.num_transitions >= capacity:
+            sac_agent.update()
+        obs = next_state
+        episode_reward += reward
+    if i % log_interval == 0:
+        sac_agent.save_models()
+
+    print(f"Episode {i} reward: {episode_reward}")
---
+++ sac/sac.py
@@ -0,0 +1,225 @@
+import os
+import numpy as np
+from collections import namedtuple
+import gymnasium as gym
+
+import torch as T
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+
+Transition = namedtuple('Transition', ['s', 'a', 'r', 's_', 'd'])
+
+class NormalizedActions(gym.ActionWrapper):
+    def _action(self, action):
+        low = self.action_space.low
+        high = self.action_space.high
+
+        action = low + (action + 1.0) * 0.5 * (high - low)
+        action = np.clip(action, low, high)
+
+        return action
+
+    def _reverse_action(self, action):
+        low = self.action_space.low
+        high = self.action_space.high
+
+        action = 2 * (action - low) / (high - low) - 1
+        action = np.clip(action, low, high)
+
+        return action
+    
+class QNetwork(nn.Module):
+    def __init__(self, n_state, n_action):
+        super(QNetwork, self).__init__()
+        self.n_state = n_state
+        self.n_action = n_action
+        self.fc1 = nn.Linear(n_state+n_action, 256)
+        self.fc2 = nn.Linear(256, 256)
+        self.fc3 = nn.Linear(256, 1)
+
+    def forward(self, state, action):
+        s = state.reshape(-1, self.n_state)
+        a = action.reshape(-1, self.n_action)
+        x = T.cat((s, a), dim=-1)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+
+        return x
+    
+class ValueNetwork(nn.Module):
+    def __init__(self, n_state):
+        super(ValueNetwork, self).__init__()
+        self.fc1 = nn.Linear(n_state, 256)
+        self.fc2 = nn.Linear(256, 256)
+        self.fc3 = nn.Linear(256, 1)
+
+    def forward(self, state):
+        x = F.relu(self.fc1(state))
+        x = F.relu(self.fc2(x))
+        value = self.fc3(x)
+
+        return value
+    
+class PolicyNetwork(nn.Module):
+    def __init__(self, n_state, min_log_std=-20, max_log_std=2):
+        super(PolicyNetwork, self).__init__()
+        self.n_state = n_state
+
+        self.fc1 = nn.Linear(n_state, 256)
+        self.fc2 = nn.Linear(256, 256)
+        self.mu_head = nn.Linear(256, 1)
+        self.log_std_head = nn.Linear(256, 1)
+
+        # self.max_action = ???
+        self.max_log_std = max_log_std
+        self.min_log_std = min_log_std
+
+
+    def forward(self, state):
+        x = F.relu(self.fc1(state))
+        x = F.relu(self.fc2(x))
+        mu = self.mu_head(x)
+        log_std_head = F.relu(self.log_std_head(x))
+        log_std_head = T.clamp(log_std_head, self.min_log_std, self.max_log_std)
+
+        return mu, log_std_head
+    
+class SAC():
+    def __init__(self, state_dim, n_action, alpha=0.0003, gamma=0.99, capacity=10000, gradient_steps=1, batch_size=64, tau=0.005, device='cpu'):
+        super(SAC, self).__init__()
+
+        self.policy = PolicyNetwork(state_dim).to(device)
+        self.value = ValueNetwork(state_dim).to(device)
+        self.target_value = ValueNetwork(state_dim).to(device)
+        self.q1 = QNetwork(state_dim, n_action).to(device)
+        self.q2 = QNetwork(state_dim, n_action).to(device)
+
+        self.policy_optim = optim.Adam(self.policy.parameters(), lr=alpha)
+        self.value_optim = optim.Adam(self.value.parameters(), lr=alpha)
+        self.q1_optim = optim.Adam(self.q1.parameters(), lr=alpha)
+        self.q2_optim = optim.Adam(self.q2.parameters(), lr=alpha)
+
+        self.gamma = gamma
+        self.device = device
+
+        self.value_criterion = nn.MSELoss()
+        self.q1_criterion = nn.MSELoss()
+        self.q2_criterion = nn.MSELoss()
+
+        self.capacity = capacity
+        self.replay_buffer = [Transition]*self.capacity
+        self.num_transitions = 0
+        self.num_training = 1
+
+        for target_param, param in zip(self.target_value.parameters(), self.value.parameters()):
+            target_param.data.copy_(param.data)
+
+        self.gradient_steps = gradient_steps
+        self.batch_size = batch_size
+        self.tau = tau
+
+        os.makedirs('./model/', exist_ok=True)
+
+    def choose_action(self, state):
+        state = T.tensor(state, dtype=T.float).unsqueeze(0).to(self.device)
+        mu, log_std = self.policy(state)
+        std = T.exp(log_std)
+        dist = T.distributions.Normal(mu, std)
+        z = dist.sample()
+        action = T.tanh(z).detach().cpu().numpy()
+        return action.item()
+    
+    def store_transition(self, s, a, r, s_, d):
+        self.replay_buffer[self.num_transitions % self.capacity] = s, a, r, s_, d
+        self.num_transitions += 1
+
+    def evaluate(self, state):
+        batch_mu, batch_std = self.policy(state)
+        batch_std = T.exp(batch_std)
+        dist = T.distributions.Normal(batch_mu, batch_std)
+        
+        z = T.distributions.Normal(0, 1).sample()
+        action = T.tanh(batch_mu + batch_std*z.to(self.device))
+        log_prob = dist.log_prob(batch_mu + batch_std*z.to(self.device)) - T.log(1 - action**2 + 1e-6)
+
+        return action, log_prob
+    
+    def update(self):
+        if self.num_training % 500 == 0:
+            print("Training .. {} times".format(self.num_training))
+
+        s = T.tensor([t.s for t in self.replay_buffer if t is not None], dtype=T.float).to(self.device)
+        a = T.tensor([t.a for t in self.replay_buffer if t is not None]).to(self.device)
+        r = T.tensor([t.r for t in self.replay_buffer if t is not None]).to(self.device)
+        s_ = T.tensor([t.s_ for t in self.replay_buffer if t is not None], dtype=T.float).to(self.device)
+        d = T.tensor([t.d for t in self.replay_buffer if t is not None]).to(self.device)
+
+        for _ in range(self.gradient_steps):
+            index = np.random.choice(self.capacity, self.batch_size, replace=False)
+            batch_s = s[index]
+            batch_a = a[index].reshape(-1, 1)
+            batch_r = r[index].reshape(-1, 1)
+            batch_s_ = s_[index]
+            batch_d = d[index].reshape(-1, 1)
+
+            target_value = self.target_value(batch_s_)
+            q_next = batch_r + self.gamma * (1 - batch_d) * target_value
+
+            expected_value = self.value(batch_s)
+            expected_q1 = self.q1(batch_s, batch_a)
+            expected_q2 = self.q2(batch_s, batch_a)
+
+            sample_action, log_prob = self.evaluate(batch_s)
+            expected_new_q = T.min(self.q1(batch_s, sample_action), self.q2(batch_s, sample_action))
+            next_value = expected_new_q - log_prob
+
+            value_loss = self.value_criterion(expected_value, next_value.detach()).mean()
+
+            q1_loss = self.q1_criterion(expected_q1, q_next.detach()).mean()
+            q2_loss = self.q2_criterion(expected_q2, q_next.detach()).mean()
+
+            pi_loss = (log_prob - expected_new_q).mean()
+
+            self.value_optim.zero_grad()
+            value_loss.backward(retain_graph=True)
+            nn.utils.clip_grad_norm_(self.value.parameters(), 0.5)
+            self.value_optim.step()
+
+            self.q1_optim.zero_grad()
+            q1_loss.backward(retain_graph=True)
+            nn.utils.clip_grad_norm_(self.q1.parameters(), 0.5)
+            self.q1_optim.step()
+
+            self.q2_optim.zero_grad()
+            q2_loss.backward(retain_graph=True)
+            nn.utils.clip_grad_norm_(self.q2.parameters(), 0.5)
+            self.q2_optim.step()
+
+            self.policy_optim.zero_grad()
+            pi_loss.backward()
+            nn.utils.clip_grad_norm_(self.policy.parameters(), 0.5)
+            self.policy_optim.step()
+
+            for target_param, param in zip(self.target_value.parameters(), self.value.parameters()):
+                target_param.data.copy_(self.tau * param.data + (1 - self.tau) * target_param.data)
+
+            self.num_training += 1
+
+    def save_model(self):
+        T.save(self.policy.state_dict(), './model/policy.pth')
+        T.save(self.value.state_dict(), './model/value.pth')
+        T.save(self.target_value.state_dict(), './model/target_value.pth')
+        T.save(self.q1.state_dict(), './model/q1.pth')
+        T.save(self.q2.state_dict(), './model/q2.pth')
+        print("Model saved ..")
+
+    def load_model(self):
+        self.policy.load_state_dict(T.load('./model/policy.pth'))
+        self.value.load_state_dict(T.load('./model/value.pth'))
+        self.target_value.load_state_dict(T.load('./model/target_value.pth'))
+        self.q1.load_state_dict(T.load('./model/q1.pth'))
+        self.q2.load_state_dict(T.load('./model/q2.pth'))
+        print("Model loaded ..")
