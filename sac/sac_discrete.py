763aed04e71aa952af04af5bfffefaf9923946de
Dikshuy <dikshant1212hsr@gmail.com>
2025-01-09 23:05:57 -0700

    He initialization for weights

--- sac/sac_discrete.py
+++ sac/sac_discrete.py
@@ -9,6 +9,11 @@ import torch.optim as optim
 from torch.distributions import Categorical
 
 from stable_baselines3.common.buffers import ReplayBuffer
+
+def hidden_init(layer):
+    fan_in = layer.weight.data.size()[0]
+    lim = 1. / np.sqrt(fan_in)
+    return (-lim, lim)
     
 class QNetwork(nn.Module):
     def __init__(self, n_state, n_action):
@@ -16,6 +21,12 @@ class QNetwork(nn.Module):
         self.fc1 = nn.Linear(n_state, 256)
         self.fc2 = nn.Linear(256, 256)
         self.fc3 = nn.Linear(256, n_action)
+        self.reset_params()
+
+    def reset_params(self):
+        self.fc1.weight.data.uniform_(*hidden_init(self.fc1))
+        self.fc2.weight.data.uniform_(*hidden_init(self.fc2))
+        self.fc3.weight.data.uniform_(-3e-3, 3e-3)
 
     def forward(self, state):
         x = F.relu(self.fc1(state))
