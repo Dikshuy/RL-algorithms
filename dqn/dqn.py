10ef01d57e7de62f75362e0c1876e08e74fc5d9e
Dikshuy <dikshant1212hsr@gmail.com>
2025-02-02 21:00:58 -0700

    dqn testing done

--- dqn/dqn.py
+++ dqn/dqn.py
@@ -1,81 +1,101 @@
-from gymnasium import ActionWrapper
 import numpy as np
-from collections import namedtuple
+import random
+from collections import namedtuple, deque
 
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-from torch.utils.data.sampler import BatchSampler, SubsetRandomSampler
+    
+class ReplayBuffer:
+    def __init__(self, buffer_size, batch_size, device):
+        self.device = device
+        self.memory = deque(maxlen=buffer_size)  
+        self.batch_size = batch_size
+        self.experience = namedtuple("Experience", field_names=["state", "action", "reward", "next_state", "done"])
 
+    def add(self, state, action, reward, next_state, done):
+        e = self.experience(state, action, reward, next_state, done)
+        self.memory.append(e)
 
-Transition = namedtuple('Transition', ('state', 'action', 'reward', 'next_state'))
+    def sample(self):
+        experiences = random.sample(self.memory, k=self.batch_size)
 
+        states = torch.from_numpy(np.stack([e.state for e in experiences if e is not None])).float().to(self.device)
+        actions = torch.from_numpy(np.vstack([e.action for e in experiences if e is not None])).float().to(self.device)
+        rewards = torch.from_numpy(np.vstack([e.reward for e in experiences if e is not None])).float().to(self.device)
+        next_states = torch.from_numpy(np.stack([e.next_state for e in experiences if e is not None])).float().to(self.device)
+        dones = torch.from_numpy(np.vstack([e.done for e in experiences if e is not None]).astype(np.uint8)).float().to(self.device)
+
+        return (states, actions, rewards, next_states, dones)
+
+    def __len__(self):
+        return len(self.memory)
+    
 class QNet(nn.Module):
-    def __init__(self, n_state, n_action):
+    def __init__(self, n_state, n_action, device):
         super(QNet, self).__init__()
         self.fc1 = nn.Linear(n_state, 128)
         self.fc2 = nn.Linear(128, 128)
         self.fc3 = nn.Linear(128, n_action)
+        self.to(device)
 
     def forward(self, x):
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         action_value = self.fc3(x)
         return action_value
-    
+
 class DQN:
-    def __init__(self, env, state_dim, action_dim, memory_capacity, batch_size, lr, gamma, target_update_interval):
+    def __init__(self, state_dim, action_dim, buffer_size, batch_size, lr, gamma, tau, device):
         super(DQN, self).__init__()
         self.state_dim = state_dim
         self.action_dim = action_dim
-        self.eval_net, self.target_net = QNet(state_dim, action_dim), QNet(state_dim, action_dim)
+        self.eval_net, self.target_net = QNet(state_dim, action_dim, device).to(device), QNet(state_dim, action_dim, device).to(device)
 
-        self.memory = [None] * memory_capacity
+        self.memory = ReplayBuffer(buffer_size, batch_size, device)
         self.memory_counter = 0
         self.batch_size = batch_size
         self.gamma = gamma
-        self.memory_capacity = memory_capacity
-        self.update_count = 0
-        self.target_update_interval = target_update_interval
+        self.tau = tau
+
+        self.device = device
 
         self.optimizer = optim.Adam(self.eval_net.parameters(), lr=lr)
         self.loss_func = nn.MSELoss()
-
+        
     def choose_action(self, state, epsilon):
-        state = torch.unsqueeze(torch.FloatTensor(state), 0)
-        action_value = self.eval_net(state)
-        action_max_value, index = torch.max(action_value, 1)
-        action = index.item()
-        if np.random.rand(1) >= epsilon:
-            action = np.random.choice(range(self.action_dim), 1).item()
+        state = torch.from_numpy(state).float().unsqueeze(0).to(self.device)
+
+        self.eval_net.eval()
+        with torch.no_grad():
+            action_value = self.eval_net(state)
+        self.eval_net.train()
+
+        if np.random.random() > epsilon:
+            action = action_value.argmax(1).item()
+        else:
+            action = np.random.choice(self.action_dim)
+        
         return action
-    
-    def store_transition(self, transition):
-        index = self.memory_counter % self.memory_capacity
-        self.memory[index] = transition
-        self.memory_counter += 1
-        return self.memory_counter >= self.memory_capacity
 
-    def learn(self):
-        if self.memory_counter >= self.memory_capacity:
-            state = torch.tensor([t.state for t in self.memory if t is not None], dtype=torch.float)
-            action = torch.tensor([t.action for t in self.memory if t is not None], dtype=torch.long).view(-1, 1)
-            reward = torch.tensor([t.reward for t in self.memory if t is not None], dtype=torch.float).view(-1, 1)
-            next_state = torch.tensor([t.next_state for t in self.memory if t is not None], dtype=torch.float)
+    def learn(self, experiences, gamma):
+        states, actions, rewards, next_states, dones = zip(*experiences)
 
-            reward = (reward - reward.mean()) / (reward.std() + 1e-7)
-            
-            with torch.no_grad():
-                q_target = reward + self.gamma * self.target_net(next_state).max(1)[0]
+        states = torch.from_numpy(np.vstack(states)).float().to(self.device)
+        actions = torch.from_numpy(np.vstack(actions)).long().to(self.device)
+        rewards = torch.from_numpy(np.vstack(rewards)).float().to(self.device)
+        next_states = torch.from_numpy(np.vstack(next_states)).float().to(self.device)
+        dones = torch.from_numpy(np.vstack(dones).astype(np.uint8)).float().to(self.device)
 
-            for index in BatchSampler(SubsetRandomSampler(range(len(self.memory))), self.batch_size, False):
-                q_eval = self.eval_net(state).gather(1, action)[index]
-                loss = self.loss_func(q_target[index].unsqueeze(1), q_eval)
+        q = self.eval_net(states).gather(1, actions)
+        q_next = self.target_net(next_states).detach().max(1)[0].unsqueeze(1)
+        q_target = rewards + gamma * q_next * (1 - dones)
 
-                self.optimizer.zero_grad()
-                loss.backward()
-                self.optimizer.step()
+        loss = self.loss_func(q, q_target)
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
 
-                if self.update_count % self.target_update_interval == 0:
-                    self.target_net.load_state_dict(self.eval_net.state_dict())
+        for target_param, param in zip(self.target_net.parameters(), self.eval_net.parameters()):
+            target_param.data.copy_(target_param.data * (1-self.tau) + param.data * self.tau)
--- dqn/main.py
+++ dqn/main.py
@@ -1,36 +1,96 @@
 import numpy as np
-from collections import namedtuple
+from collections import deque
 import gym
-
+import torch
+import random
 from dqn import DQN
 
-env = gym.make('CartPole-v1')
-state_dim = env.observation_space.shape[0]
-action_dim = env.action_space.n
-memory_capacity = 10000
-batch_size = 256
-lr = 3e-4
-gamma = 0.99
-target_update_interval = 100
-num_episodes = 2000
 
-Transition = namedtuple('Transition', ('state', 'action', 'reward', 'next_state'))
+def evaluate_policy(env, agent: DQN, turns = 3):
+	total_scores = 0
+	for _ in range(turns):
+		obs, _ = env.reset()
+		done = False
+		while not done:
+			action = agent.choose_action(obs, epsilon=0.0)
+			obs_next, reward, terminated, truncated, _ = env.step(action)
+			done = terminated or truncated
 
-agent = DQN(env, state_dim, action_dim, memory_capacity, batch_size, lr, gamma, target_update_interval)
+			total_scores += reward
+			obs = obs_next
+               
+	return int(total_scores/turns)
 
-for i in range(num_episodes):
-    state, _ = env.reset()
-    done = False
-    score = 0
-    while not done:
-        action = agent.choose_action(state, 0.9)
-        next_state, reward, terminated, truncated, _ = env.step(action)
-        done = terminated or truncated
-        agent.store_transition(Transition(state, action, reward, next_state))
-        agent.learn()
-        state = next_state
-        score += reward
+def train_dqn(seed):
+    env = gym.make('CartPole-v1')
+    env_eval = gym.make('CartPole-v1')
 
-    print(f'Episode {i}, score: {score}')
+    env_seed = seed
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
 
-env.close()
+    state_dim = env.observation_space.shape[0]
+    action_dim = env.action_space.n
+    buffer_size = 10000
+    batch_size = 128
+    lr = 3e-4
+    gamma = 0.99
+    tau = 1e-3
+    eps_start = 1.0
+    eps_end = 0.1
+    eps_decay_rate = 0.99
+    num_episodes = 500
+    eval_interval = 100
+    buffer = deque(maxlen=buffer_size)
+
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+    agent = DQN(state_dim, action_dim, buffer_size, batch_size, lr, gamma, tau, device)
+
+    returns = []
+
+    total_steps = 0
+
+    for i in range(num_episodes):
+        obs, _ = env.reset(seed=env_seed)
+        epsilon = max(eps_end, eps_start * (eps_decay_rate ** i))
+
+        env_seed += 1
+        done = False
+        episodic_reward = 0
+
+        while not done:
+            action = agent.choose_action(obs, epsilon)
+            next_obs, reward, terminated, truncated, _ = env.step(action)
+            done = terminated or truncated
+            
+            buffer.append((obs, action, reward, next_obs, done))
+            
+            obs = next_obs
+            episodic_reward += reward
+            
+            if len(buffer) >= batch_size:
+                experiences = random.sample(buffer, batch_size)
+                agent.learn(experiences, gamma)
+
+            if total_steps % eval_interval == 0:
+                eval_reward = evaluate_policy(env_eval, agent)
+                print(f"Step: {total_steps}, Eval reward: {eval_reward}")
+
+            total_steps += 1
+
+        returns.append(episodic_reward)
+
+    env.close()
+    env_eval.close()
+
+    return returns
+
+if __name__ == "__main__":
+    num_seeds = 2
+    seeds = [i for i in range(num_seeds)]
+    
+    for seed in seeds:
+        returns = train_dqn(seed)
