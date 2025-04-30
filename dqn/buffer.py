335d983c8fa6233dd8343e3769385c5f58a441dd
Dikshuy <dikshant1212hsr@gmail.com>
2025-04-30 15:20:02 -0600

    added PER buffer

--- dqn/buffer.py
+++ dqn/buffer.py
@@ -1,5 +1,6 @@
 import random
 from collections import namedtuple, deque
+import re
 import torch
 import numpy as np
 
@@ -54,6 +55,7 @@ class ReplayBuffer:
     def __len__(self):
         return len(self.memory)
 
+# logic from cleanrl 
 class SumSegmentTree:
     def __init__(self, capacity):
         self.capacity = capacity
@@ -66,7 +68,7 @@ class SumSegmentTree:
             self.tree[parent] = self.tree[2 * parent + 1] + self.tree[2 * parent + 2]
             parent = (parent - 1) // 2
 
-    def update(self, idx, value):
+    def _update(self, idx, value):
         idx += self.capacity - 1
         self.tree[idx] = value
         self._propagate(idx)
@@ -74,7 +76,7 @@ class SumSegmentTree:
     def total(self):
         return self.tree[0]
     
-    def retrieve(self, value):
+    def _retrieve(self, value):
         idx = 0
         while idx * 2 + 1 < self.tree_size:
             left = idx * 2 + 1
@@ -99,7 +101,7 @@ class MinSegmentTree:
             self.tree[parent] = min(self.tree[2 * parent + 1], self.tree[2 * parent + 2])
             parent = (parent - 1) // 2
 
-    def update(self, idx, value):
+    def _update(self, idx, value):
         idx += self.capacity - 1
         self.tree[idx] = value
         self._propagate(idx)
@@ -134,4 +136,84 @@ class PrioritizedReplayBuffer:
 
         self.n_step_buffer = deque(maxlen=n_step)
 
-        self.PriorityBatch = namedtuple("PrioritizedBatch", ["observations", "actions", "rewards", "next_observations", "dones", "indices", "weights"])
+    def _get_n_step_info(self):
+        reward = 0.0
+        next_obs = self.n_step_buffer[-1][3]
+        done = self.n_step_buffer[-1][4]
+
+        for i in range(len(self.n_step_buffer)):
+            reward += (self.gamma ** i) * self.n_step_buffer[i][2]
+            if self.n_step_buffer[i][4]:
+                next_obs = self.n_step_buffer[i][3]
+                done = True
+                break
+        return reward, next_obs, done
+    
+    def add(self, obs, action, reward, next_obs, done):
+        self.n_step_buffer.append((obs, action, reward, next_obs, done))
+
+        if len(self.n_step_buffer) < self.n_step:   
+            return
+        
+        reward, next_obs, done = self._get_n_step_info()
+        obs = self.n_step_buffer[0][0]
+        action = self.n_step_buffer[0][1]
+
+        idx = self.pos
+        self.buffer_obs[idx] = obs
+        self.buffer_actions[idx] = action
+        self.buffer_rewards[idx] = reward
+        self.buffer_next_obs[idx] = next_obs
+        self.buffer_dones[idx] = done
+
+        priority = self.max_priority ** self.alpha
+        self.sum_tree._update(idx, priority)
+        self.min_tree._update(idx, priority)
+
+        self.pos = (self.pos + 1) % self.capacity
+        self.size = min(self.size + 1, self.capacity)
+
+        if done:
+            self.n_step_buffer.clear()
+
+    def sample(self, batch_size):
+        indices = []
+        p_total = self.sum_tree.total()
+        segment = p_total / batch_size
+
+        for i in range(batch_size):
+            a = segment * i
+            b = segment * (i + 1)
+            upperbound = np.random.uniform(a, b)
+            idx = self.sum_tree._retrieve(upperbound)
+            indices.append(idx)
+
+        samples = {
+            "observations": torch.from_numpy(self.buffer_obs[indices]).to(self.device),
+            "actions": torch.from_numpy(self.buffer_actions[indices]).to(self.device).unsqueeze(1),
+            "rewards": torch.from_numpy(self.buffer_rewards[indices]).to(self.device).unsqueeze(1),
+            "next_observations": torch.from_numpy(self.buffer_next_obs[indices]).to(self.device),
+            "dones": torch.from_numpy(self.buffer_dones[indices]).to(self.device).unsqueeze(1),
+        }
+
+        probs = np.array([self.sum_tree.tree[idx + self.capacity - 1] for idx in indices])
+        weights = (self.size * probs / self.sum_tree.total()) ** (-self.beta)
+        weights = weights / weights.max()
+        samples["weights"] = torch.from_numpy(weights).to(self.device).unsqueeze(1)
+        samples["indices"] = indices
+
+        return PrioritizedBatch(**samples)
+    
+    def _update_priorities(self, indices, priorities):
+        priorities = np.abs(priorities) + self.eps
+        self.max_priority = max(self.max_priority, priorities.max())
+
+        for idx, priority in zip(indices, priorities):
+            priority = priority ** self.alpha
+            self.sum_tree._update(idx, priority)
+            self.min_tree._update(idx, priority)
+
+    def __len__(self):
+        return self.size
+    
+PrioritizedBatch = namedtuple("PrioritizedBatch", ["observations", "actions", "rewards", "next_observations", "dones", "indices", "weights"])
